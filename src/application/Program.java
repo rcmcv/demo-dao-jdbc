c6d479e0bfb5853f1085706439d17a739d44b33c
rcmcv <rcmcv.producoes@gmail.com>
2023-08-30 16:48:08 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,7 +33,14 @@ public class Program {
 		System.out.println();
 		System.out.println("==== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg Red", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
+		//sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println();
+		System.out.println("==== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Paul Black");
+		seller.setEmail("paul@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
