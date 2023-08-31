d7b491991cdcd56f3f58380670107f6e22867b7c
rcmcv <rcmcv.producoes@gmail.com>
2023-08-31 12:24:57 -0300

    DepartmentDao implementation and test

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -85,6 +85,7 @@ public class SellerDaoJDBC implements SellerDao {
 	public void deleteById(Integer id) {
 
 		PreparedStatement st = null;
+		
 		try {
 			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 			st.setInt(1, id);
